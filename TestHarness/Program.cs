0a7ad2fdbc8a901e425808fa19375d391e1513f3
Craig Shaw <craig@craigshaw.com>
2015-07-30 23:00:44 +0100

    Added ROM validation

--- TestHarness/Program.cs
+++ TestHarness/Program.cs
@@ -22,27 +22,34 @@ namespace TestHarness
 
         private async void Run(string romPath)
         {
-            // Load ROM content
-            ROM rom = new ROM(new ROMLoader(romPath));
-            await rom.LoadAsync();
+            try
+            {
+                // Load ROM content
+                ROM rom = new ROM(new ROMLoader(romPath));
+                await rom.LoadAsync();
 
-            Console.WriteLine("System is {0} endian", BitConverter.IsLittleEndian ? "little" : "big");
-            Console.WriteLine("Loaded ROM {0}", romPath);
+                Console.WriteLine("System is {0} endian", BitConverter.IsLittleEndian ? "little" : "big");
+                Console.WriteLine("Loaded ROM {0}", romPath);
 
-            // Get current checksum
-            Console.WriteLine("Checksum: 0x{0:X}", rom.Checksum);
+                // Get current checksum
+                Console.WriteLine("Checksum: 0x{0:X}", rom.Checksum);
 
-            // Calculated checksum
-            Console.WriteLine("Calculated Checksum: 0x{0:X}", rom.CalculateChecksum());
+                // Calculated checksum
+                Console.WriteLine("Calculated Checksum: 0x{0:X}", rom.CalculateChecksum());
 
-            // Get, then update the master code
-            UInt16 masterCode = rom.GetValue<UInt16>(0xFF888);
-            Console.WriteLine("Current Master Code: 0x{0:X}", masterCode);
+                // Get, then update the master code
+                UInt16 masterCode = rom.GetValue<UInt16>(0xFF888);
+                Console.WriteLine("Current Master Code: 0x{0:X}", masterCode);
 
-            UInt16 check = rom.GetValue<UInt16>(0x18E);
-            Console.WriteLine("Checksum (from generic method): 0x{0:X}", check);
+                UInt16 check = rom.GetValue<UInt16>(0x18E);
+                Console.WriteLine("Checksum (from generic method): 0x{0:X}", check);
 
-            //await rom.SaveAsync();
+                //await rom.SaveAsync();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+            }
         }
     }
 
