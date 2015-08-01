93bd111e123a8d170d2d0ab16167ec2c3783e314
Craig Shaw <craig@craigshaw.com>
2015-08-01 15:27:28 +0100

    Added tests and started to refactor code out of ROM

---
+++ MegadriveUtilities.Tests/BigEndianBinaryAccessorTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace MegadriveUtilities.Tests
+{
+    [TestClass]
+    public class BigEndianBinaryAccessorTests
+    {
+        [TestMethod]
+        public void ConstructsWithByteArray()
+        {
+            BigEndianBinaryAccessor accessor = new BigEndianBinaryAccessor(Encoding.ASCII.GetBytes("Testing construction"));
+            Assert.IsNotNull(accessor);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowsWhenConstructedWithNull()
+        {
+            BigEndianBinaryAccessor accessor = new BigEndianBinaryAccessor(null);
+        }
+
+        [TestMethod]
+        public void GetsUInt16()
+        {
+            byte[] testData = new byte[] { 0xea, 0x60 };
+            BigEndianBinaryAccessor accessor = new BigEndianBinaryAccessor(testData);
+            UInt16 result = accessor.GetUInt16(0);
+            Assert.AreEqual(60000, result);
+        }
+
+        [TestMethod]
+        public void GetsInt16()
+        {
+            byte[] testData = new byte[] { 0xD8, 0xF0 };
+            BigEndianBinaryAccessor accessor = new BigEndianBinaryAccessor(testData);
+            Int16 result = accessor.GetInt16(0);
+            Assert.AreEqual(-10000, result);
+        }
+
+        [TestMethod]
+        public void GetsUInt32()
+        {
+            UInt32 expectedResult = 2147483647;
+            byte[] testData = new byte[] { 0x7F, 0xFF, 0xFF, 0xFF };
+            BigEndianBinaryAccessor accessor = new BigEndianBinaryAccessor(testData);
+            UInt32 result = accessor.GetUInt32(0);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void GetsInt32()
+        {
+            Int32 expectedResult = -2000000;
+            byte[] testData = new byte[] { 0xFF, 0xE1, 0x7B, 0x80 };
+            BigEndianBinaryAccessor accessor = new BigEndianBinaryAccessor(testData);
+            Int32 result = accessor.GetInt32(0);
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetXIntXXThrowsIfOffsetIsInvalid()
+        {
+            BigEndianBinaryAccessor accessor = new BigEndianBinaryAccessor(new byte[] { 0xFF, 0xFE, 0xFD });
+            Int16 invalid = accessor.GetInt16(UInt16.MaxValue);
+        }
+
+        [TestMethod]
+        public void CompareBytesAtReturnsTrueWhenMatchedBytes()
+        {
+            string testData = "Some test data for this unit test";
+            string match = "unit test";
+            BigEndianBinaryAccessor accessor = new BigEndianBinaryAccessor(Encoding.ASCII.GetBytes(testData));
+            Assert.IsTrue(accessor.CompareBytesAt(0x18, Encoding.ASCII.GetBytes(match)));
+        }
+
+        [TestMethod]
+        public void CompareBytesAtReturnsFasleWhenNotMatching()
+        {
+            string testData = "Some test data for this unit test";
+            string match = "unit test";
+            BigEndianBinaryAccessor accessor = new BigEndianBinaryAccessor(Encoding.ASCII.GetBytes(testData));
+            Assert.IsFalse(accessor.CompareBytesAt(0x10, Encoding.ASCII.GetBytes(match)));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CompareBytesThrowsArgumentExceptionIfInvalidOffsetGiven()
+        {
+            string testData = "Some test data for this unit test";
+            BigEndianBinaryAccessor accessor = new BigEndianBinaryAccessor(Encoding.ASCII.GetBytes(testData));
+            Assert.IsFalse(accessor.CompareBytesAt(0xFFFF, Encoding.ASCII.GetBytes("test")));
+        }
+    }
+}
---
+++ MegadriveUtilities/BigEndianBinaryAccessor.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MegadriveUtilities
+{
+    public class BigEndianBinaryAccessor
+    {
+        private byte[] binData;
+
+        public BigEndianBinaryAccessor(byte[] binData)
+        {
+            if (binData == null)
+                throw new ArgumentNullException("binData");
+
+            this.binData = binData;
+        }
+
+        // Can't get this to worked with signed values, int16 and int32
+        //public T GetValue<T>(uint offset) where T : struct
+        //{
+        //    int typeSize = Marshal.SizeOf(typeof(T));
+        //    UInt64 temp = 0;
+
+        //    for (int i = typeSize - 1, j = 0; i >= 0; i--, j++)
+        //    {
+        //        temp |= (UInt64)binData[offset + i] << (j << 3);
+        //    }
+
+        //    return (T)Convert.ChangeType(temp, typeof(T));
+        //}
+
+        public UInt16 GetUInt16(uint offset)
+        {
+            return GetValueFromROM<UInt16>(offset, (data, index) => BitConverter.ToUInt16(data, index));
+        }
+
+        public Int16 GetInt16(uint offset)
+        {
+            return GetValueFromROM<Int16>(offset, (data, index) => BitConverter.ToInt16(data, index));
+        }
+
+        public UInt32 GetUInt32(uint offset)
+        {
+            return GetValueFromROM<UInt32>(offset, (data, index) => BitConverter.ToUInt32(data, index));
+        }
+
+        public Int32 GetInt32(uint offset)
+        {
+            return GetValueFromROM<Int32>(offset, (data, index) => BitConverter.ToInt32(data, index));
+        }
+
+        public unsafe bool CompareBytesAt(uint offset, byte[] compareTo)
+        {
+            if (offset >= binData.Length)
+                throw new ArgumentException("offset >= data length", "offset");
+
+            fixed (byte* arrayPtr = &binData[offset])
+            {
+                byte* currentByte = arrayPtr;
+                for (int i = 0; i < compareTo.Length; i++)
+                {
+                    if (*currentByte != compareTo[i])
+                        return false;
+
+                    currentByte++;
+                }
+            }
+
+            return true;
+        }
+
+        private T GetValueFromROM<T>(uint offset, Conversion<T> conversion)
+        {
+            if (offset >= binData.Length)
+                throw new ArgumentException("offset >= data length", "offset");
+
+            int typeSize = Marshal.SizeOf(typeof(T));
+            byte[] bytes = new byte[typeSize];
+            Array.Copy(binData, offset, bytes, 0, typeSize);
+            Array.Reverse(bytes);
+            return conversion(bytes, 0);
+        }
+    }
+}
